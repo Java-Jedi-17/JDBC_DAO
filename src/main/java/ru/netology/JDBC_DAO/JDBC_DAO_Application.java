0c1dfb3f8a49bf0f79e9bf97a324693d6719c96a
JavaJedi <aleksk170400@gmail.com>
2024-11-29 03:20:41 +0500

    Initial commit

--- src/main/java/ru/netology/JDBC_DAO/JDBC_DAO_Application.java
+++ src/main/java/ru/netology/JDBC_DAO/JDBC_DAO_Application.java
@@ -1,4 +1,13 @@
 package ru.netology.JDBC_DAO;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JDBC_DAO_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JDBC_DAO_Application.class, args);
+    }
+
 }
--- src/main/java/ru/netology/JDBC_DAO/service/Service.java
+++ src/main/java/ru/netology/JDBC_DAO/service/Service.java
@@ -1,4 +1,19 @@
 package ru.netology.JDBC_DAO.service;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.netology.JDBC_DAO.repository.Repository;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
 public class Service {
+    Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<String> getProducts(String name) {
+        return repository.getProductName(name);
+    }
 }
